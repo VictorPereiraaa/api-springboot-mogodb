71508fb145384e7a726ec9a50a8e151000fcba38
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-10 22:59:35 -0300

    Endpoint to return user posts

--- src/main/java/com/victorpereira/apisbmongo/resources/UserResource.java
+++ src/main/java/com/victorpereira/apisbmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.victorpereira.apisbmongo.domain.Post;
 import com.victorpereira.apisbmongo.domain.User;
 import com.victorpereira.apisbmongo.dto.UserDTO;
 import com.victorpereira.apisbmongo.services.UserService;
@@ -36,8 +37,8 @@ public class UserResource {
 	
 	@GetMapping(value="/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		User user = service.findById(id);
-		return ResponseEntity.ok().body(new UserDTO(user));
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 	
 	@PostMapping
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
